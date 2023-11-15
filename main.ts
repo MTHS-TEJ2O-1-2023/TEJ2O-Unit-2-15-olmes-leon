951630694b6757b101b268fefcc1fee4ae54fc45
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-11-15 13:30:30 -0500

    code

--- main.ts
+++ main.ts
@@ -1,36 +1,40 @@
 /* Copyright (c) 2023 MTHS All rights reserved
  *
  * Created by: Olmes
- * Created on: Nov 2023
- * This program uses while loops, that if "A" button is pressed, the pixels moves around the edge of the Microbit
+ * Created on: Nov 15 2023
+ * This program This program uses while loops, that if "A" button is pressed, the pixels moves around the edge of the Microbit
 */
 
 // variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
 let ones: number = 0
 let tens: number = 0
 
 // setup
-game.createSprite(0, 0)
+basic.showIcon(IconNames.Duck)
+basic.pause(500)
+basic.clearScreen()
 
-// when "A" is pressed, the pixels move down in a diagnol
+// when "A" is pressed, 
 input.onButtonPressed(Button.A, function () {
-  
-  // setup
-  basic.clearScreen()
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
 
-  tens = 0
-  while (tens <= 9) {
-    ones = 0
-    while (ones <= 9) {
-        basic.showString(tens.toString() + ones.toString())
-        ones++
+    tens = 0
+    while (tens <= 4) {
+        ones = 0
+        while (ones <= 4) {
+            sprite.move(1)
+            ones++
+        }
+        tens++
+        sprite.turn(Direction.Right, 90)
     }
-    tens++
-  }
 
-  basic.pause(500)
-  basic.showIcon(IconNames.Duck)
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
 })
-
-// sprite.move(1) 
-// sprite.turn(Direction.Right, 90)
