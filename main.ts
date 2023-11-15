11584851228f82be5b8e3c7c4fcb4801bb5d53cd
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-11-15 13:12:02 -0500

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Olmes
+ * Created on: Nov 2023
+ * This program uses while loops, that if "A" button is pressed, the pixels moves around the edge of the Microbit
 */
 
-basic.showString('Hello, World!')
+// variables
+let ones: number = 0
+let tens: number = 0
+
+// setup
+game.createSprite(0, 0)
+
+// when "A" is pressed, the pixels move down in a diagnol
+input.onButtonPressed(Button.A, function () {
+  
+  // setup
+  basic.clearScreen()
+
+  tens = 0
+  while (tens <= 9) {
+    ones = 0
+    while (ones <= 9) {
+        basic.showString(tens.toString() + ones.toString())
+        ones++
+    }
+    tens++
+  }
+
+  basic.pause(500)
+  basic.showIcon(IconNames.Duck)
+})
+
+// sprite.move(1) 
+// sprite.turn(Direction.Right, 90)
